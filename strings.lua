227987a7b5842d15cc7e7d5f805ddf481fc999ae
Florian Thiel <flth@flth.de>
2024-12-19 22:03:40 +0100

    Bahn > Bundesbahn

--- strings.lua
+++ strings.lua
@@ -3,7 +3,7 @@ return {
 	en = {
 
 		["mod_name"] = "Sliding roof wagon Taems 890 / Taehms 891",
-		["mod_desc"] = "The Taems 890 and Taehms 891 were 4-axle sliding roof wagons used by Deutsche Bahn from the 1960s onward to transport moisture-sensitive and bulky goods. Built between 1971 and 1975, some Taehms 891 wagons were retrofitted in 1973/74 with loading frames for carrying steel coils with a single load capacity of up to 32 tons. Both wagon types featured protective sliding roofs and remained in service until the 2000s, when they were gradually phased out.",
+		["mod_desc"] = "The Taems 890 and Taehms 891 were 4-axle sliding roof wagons used by Deutsche Bundesbahn from the 1960s onward to transport moisture-sensitive and bulky goods. Built between 1971 and 1975, some Taehms 891 wagons were retrofitted in 1973/74 with loading frames for carrying steel coils with a single load capacity of up to 32 tons. Both wagon types featured protective sliding roofs and remained in service until the 2000s, when they were gradually phased out.",
 		["taems890_desc"] = "The Taems 890 are 4-axle swing-roof wagons, 831 of which were built between 1966 and 1970. They were used to transport moisture-sensitive goods, later also specifically for alumina. They were gradually phased out from 1997; the last wagon was in service until 2011.",
 		["taehms891_desc"] = "The Taehms 891, of which 1,679 were built between 1971 and 1975, were characterized by automatic load braking. This version was a converted version for coil transportation. The last examples were retired from the fleet in 2001",
 		["sound_check"] = "Activate custom sound?",
@@ -17,7 +17,7 @@ return {
 	de = {
 
 		["mod_name"] = "Schwenkdachwagen Taems 890 / Taehms 891",
-		["mod_desc"] = "Die Taems 890 und Taehms 891 waren 4-achsige Schwenkdachwagen der Deutschen Bahn, die ab den 1960er Jahren für den Transport von nässeempfindlichen und sperrigen Gütern eingesetzt wurden. Die Taehms 891, gebaut von 1971 bis 1975, erhielten 1973/74 teilweise Ladegestelle für den Transport von Stahl-Coils mit einer Einzellast von bis zu 32 Tonnen. Beide Bauarten zeichneten sich durch ein schützendes Schwenkdach aus und blieben bis in die 2000er Jahre im Einsatz, bevor sie schrittweise ausgemustert wurden.",
+		["mod_desc"] = "Die Taems 890 und Taehms 891 waren 4-achsige Schwenkdachwagen der Deutschen Bundesbahn, die ab den 1960er Jahren für den Transport von nässeempfindlichen und sperrigen Gütern eingesetzt wurden. Die Taehms 891, gebaut von 1971 bis 1975, erhielten 1973/74 teilweise Ladegestelle für den Transport von Stahl-Coils mit einer Einzellast von bis zu 32 Tonnen. Beide Bauarten zeichneten sich durch ein schützendes Schwenkdach aus und blieben bis in die 2000er Jahre im Einsatz, bevor sie schrittweise ausgemustert wurden.",
 		["taems890_desc"] = "Die Taems 890 sind 4-achsige Schwenkdachwagen, die von 1966 bis 1970 in 831 Exemplaren gebaut wurden. Sie dienten dem Transport von nässeempfindlichen Gütern, später auch speziell für Tonerde. Ab 1997 wurden sie schrittweise ausgemustert; der letzte Wagen war bis 2011 im Einsatz.",
 		["taehms891_desc"] = "Die Taehms 891, von 1971 bis 1975 in 1.679 Exemplaren gebaut, zeichneten sich durch eine selbsttätige Lastabbremsung aus. Dabei bezeichnete diese Version eine für Coil-Transporte umgerüstete Version. Letzte Exemplare schieden 2001 aus dem Bestand aus.",
 		["sound_check"] = "Eigenes Soundset aktivieren?",
